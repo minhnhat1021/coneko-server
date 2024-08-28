0da6863fcfa351af44266089531d445fd384d08e
minhnhat <minhnhat.dev.21@gmail.com>
2024-08-28 21:19:05 +0700

    fix date 1

--- src/app/controllers/RoomController.js
+++ src/app/controllers/RoomController.js
@@ -14,7 +14,17 @@ class RoomsController {
 
     async roomPayment(req, res, next) {
         try{
-            const {startDate, endDate, days, totalPrice, roomId, userId} = req.body
+            let { startDate, endDate, days, totalPrice, roomId, userId } = req.body
+
+            // Chuyển đổi Date sang đúng định dạng giờ Việt Nam
+            const convertDate = (date) => {
+                const vietnamTimezoneOffset = 7 * 60
+                const vietnamDate = new Date(new Date(date).getTime() + vietnamTimezoneOffset * 60 * 1000)
+                return vietnamDate
+            }
+            startDate = convertDate(startDate)
+            endDate = convertDate(endDate)
+            console.log(startDate, endDate)
 
             // Kiểm tra user
             const user = await User.findById(userId)
@@ -37,7 +47,7 @@ class RoomsController {
                 amountSpent: totalPrice
             })
 
-            // Cập nhật phòng đang có quyền sử dụng
+            // Cập nhật phòng mà khách hàng đang có quyền sử dụng
             user.currentRooms.push({
                 roomId,
                 checkInDate: startDate,
