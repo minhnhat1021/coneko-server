87fbfc87e577c4392a00b6caea11e77ebaccfc45
minhnhat <minhnhat.dev.21@gmail.com>
2024-09-25 00:04:06 +0700

    update thanh toán paypal

--- src/app/controllers/CheckoutController.js
+++ src/app/controllers/CheckoutController.js
@@ -1,7 +1,7 @@
 const Room = require('../models/Room')
 const User = require('../models/User')
 
-const paypal = require('paypal-rest-sdk');
+const paypal = require('paypal-rest-sdk')
 
 // Cấu hình SDK với thông tin từ PayPal Developer
 paypal.configure({
@@ -83,7 +83,12 @@ class RoomsController {
 
     // Paypal check out -------------------------------------------------------------------
     payPalCheckout(req, res, next) {
-        const { totalPrice,roomId, userId, roomName } = req.body
+        let { 
+            startDate, endDate, days, roomPrice, roomCharge, amenitiesPrice, 
+            amenitiesCharge, amenities,  totalPrice, roomId, userId 
+        } = req.body
+
+        const paymentDetails = encodeURIComponent(JSON.stringify(req.body))
         // Cấu hình yêu cầu thanh toán
         const exchangeRate = 24605
         const amountInUSD = (totalPrice / exchangeRate).toFixed(2)
@@ -98,10 +103,10 @@ class RoomsController {
                     "currency": "USD",
                     "total": amountInUSD.toString()
                 },
-                "description": "This is the payment transaction description."
+                "description": `Thanh toán bằng payPal`
             }],
             "redirect_urls": {
-                "return_url": `http://localhost:3000/payment-successful`,  // URL khi thanh toán thành công
+                "return_url": `http://localhost:3000/payment-successful?paymentDetails=${paymentDetails}`,  // URL khi thanh toán thành công
                 "cancel_url": "http://localhost:3000/payment-cancel"    // URL khi thanh toán bị hủy
             }
         }
@@ -112,8 +117,9 @@ class RoomsController {
                 console.error(error)
                 res.status(500).json({ error: 'Error creating PayPal payment' })
             } else {
+
                 // Trả về URL để khách hàng thực hiện thanh toán trên PayPal
-                for (let i = 0; i < payment.links.length; i++) {
+                for (let i = 0 ; i < payment.links.length ; i++) {
                     if (payment.links[i].rel === 'approval_url') {
                         res.json({ data: { paymentUrl: payment.links[i].href } })
                         break
@@ -126,33 +132,91 @@ class RoomsController {
 
     // confirm Paypal check out -------------------------------------------------------------------
 
-    confirmPayPalCheckout = (req, res) => {
-        const {totalPrice, paymentId, payerId } = req.body;
-        
-        const exchangeRate = 24605
-        const amountInUSD = (totalPrice / exchangeRate).toFixed(2)
+    async confirmPayPalCheckout (req, res, next) {
 
-        const execute_payment_json = {
-            "payer_id": payerId,
-            "transactions": [{
-                "amount": {
-                    "currency": "USD",
-                    "total": amountInUSD.toString() 
-                }
-            }]
-        };
-    
-        // Gọi PayPal API để xác nhận thanh toán
-        paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-            if (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Error confirming PayPal payment' });
-            } else {
-                console.log("Payment confirmed:", JSON.stringify(payment));
-                res.json({data: { message: 'Payment successful', payment }});
+        try{
+            const { 
+                startDate, endDate, days, roomPrice, roomCharge, amenitiesPrice, 
+                amenitiesCharge, amenities,  totalPrice, roomId, userId, paymentId, payerId
+            } = req.body
+            
+            // Lưu thông tin đặt phòng vào user và room
+            const user = await User.findById(userId)
+            if (!user) {
+                return res.status(404).json({ msg: 'User đang thanh toán không khả dụng' })
             }
-        });
+
+            user.totalSpent += totalPrice
+
+            const bookingDate = Date.now()
+            // Handle model User ----------------------------------------------------------------
+
+            // Thêm giao dịch vào lịch sử phòng đã đặt
+            user.bookedRooms.push({
+                roomId, checkInDate: startDate, checkOutDate: endDate, days,
+                roomPrice, roomCharge, amenitiesPrice, amenitiesCharge, amenities, 
+                amountSpent: totalPrice,
+                bookingDate
+            })
+
+            // Cập nhật phòng mà khách hàng đang có quyền sử dụng
+            user.currentRooms.push({
+                roomId, checkInDate: startDate, checkOutDate: endDate, days,
+                roomPrice, roomCharge, amenitiesPrice, amenitiesCharge, amenities, 
+                amountSpent: totalPrice,
+                bookingDate
+            })
+
+            // Handle model Room ----------------------------------------------------------------
+            const room = await Room.findById(roomId)
+
+            // Thêm giao dịch vào lịch sử phòng đã đặt
+            room.bookedUsers.push({
+                userId
+            })
+
+            // Cập nhật phòng đang có quyền sử dụng
+            room.currentUsers.push({
+                userId,
+                checkInDate: startDate,
+                checkOutDate: endDate,
+            })
+
+            // Gọi PayPal API để xác nhận thanh toán ----------------------------------------------------------------
+            const exchangeRate = 24605
+            const amountInUSD = (totalPrice / exchangeRate).toFixed(2)
+
+            const execute_payment_json = {
+                "payer_id": payerId,
+                "transactions": [{
+                    "amount": {
+                        "currency": "USD",
+                        "total": amountInUSD.toString() 
+                    }
+                }]
+            }
+            
+            paypal.payment.execute(paymentId, execute_payment_json, async function (error, payment) {
+                if (error) {
+                    console.error(error)
+                    res.status(500).json({ error: 'Lỗi xác nhận thanh toán payPal' })
+                } else {
+                    try {
+                        await user.save()
+                        await room.save()
+                        return res.json({ data: { message: 'Thanh toán PayPal thành công', payment } })
+
+                    } catch (saveError) {
+                        console.error('Lỗi khi lưu thông tin người dùng hoặc phòng:', saveError)
+                        return res.status(500).json({ error: 'Lỗi khi lưu thông tin vào cơ sở dữ liệu' })
+                    }
+                }
+            })
+        } catch(err) {
+            next(err)
+        }
     }
 }
 
 module.exports = new RoomsController
+
