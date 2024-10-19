de42068d9fc1e3d2c02b42b4d8e5874c57fc5403
minhnhat <minhnhat.dev.21@gmail.com>
2024-10-19 22:59:31 +0700

    ..

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/coneko_hotel_dev', {
+        await mongoose.connect('mongodb://localhost:27017/coneko_hotel', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
